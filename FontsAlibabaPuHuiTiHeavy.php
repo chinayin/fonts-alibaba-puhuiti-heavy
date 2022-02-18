705aab31b442aacf2f12467409fccb0a29eb1085
lei.tian <whereismoney@qq.com>
2022-02-18 17:42:48 +0800

    更多字体库

---
+++ FontsAlibabaPuHuiTiHeavy.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Fonts\AlibabaPuHuiTi\Heavy;
+
+class FontsAlibabaPuHuiTiHeavy
+{
+
+    /**
+     * @return string
+     */
+    public static function getPath(): string
+    {
+        return __DIR__ . 'Alibaba-PuHuiTi-Heavy.ttf';
+    }
+
+
+}
